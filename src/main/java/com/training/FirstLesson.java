ba48515d61b6d908229f381a1191305126c677e6
Irina Rylova <irinarylova@Irinas-Air.fios-router.home>
2021-03-04 13:10:07 -0500

    Initial commit

---
+++ src/main/java/com/training/FirstLesson.java
@@ -0,0 +1,7 @@
+package com.training;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+    }
+}
