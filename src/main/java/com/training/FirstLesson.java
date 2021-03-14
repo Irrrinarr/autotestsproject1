369c25bd0acff9251779e627141abe60a8e08e97
Irine <80062838+Irrrinarr@users.noreply.github.com>
2021-03-14 15:13:58 -0400

    Delete FirstLesson.java

--- src/main/java/com/training/FirstLesson.java
+++
@@ -1,7 +0,0 @@
-package com.training;
-
-public class FirstLesson {
-    public static void main(String[] args) {
-//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-    }
-}
